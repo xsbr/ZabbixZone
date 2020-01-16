ee6a2f8aa5dcc32db0b5dee56434a5eddec2c8a0
Jens Berthold <jens@jebecs.de>
2020-01-16 15:45:42 +0100

    Fix get-table-list.pl to work with Git repository

--- get-table-list.pl
+++ get-table-list.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/env perl
 # NAME
-#     get-table-list.pl - List current and historic Zabbix database tables 
+#     get-table-list.pl - List current and historic Zabbix database tables
 #
 # SYNOPSIS
 #     This is mainly a helper script for developing the backup script.
 #
-#     It connects to svn://svn.zabbix.com (using Subversion client "svn") and 
+#     It connects to svn://svn.zabbix.com (using Subversion client "svn") and
 #     fetches the schema definitions of all tagged Zabbix versions beginning
 #     from 1.3.1 (this takes a while).
 #
@@ -16,28 +16,30 @@
 #     v0.1 - 2014-09-19 First version
 #
 # AUTHOR
-#     Jens Berthold (maxhq), 2014
+#     Jens Berthold (maxhq), 2020
+use strict;
+use warnings;
 
-use File::Temp qw/ tempfile /;
+use version;
 
+my $REPO = 'https://git.zabbix.com/scm/zbx/zabbix.git';
+my $REPO_WEB = 'https://git.zabbix.com/projects/ZBX/repos/zabbix/raw';
 my $tables = {}; # for each table, create a list of Zabbix versions that know it
-my (undef, $tmpfile) = tempfile();
 
 sub stop {
 	my ($msg) = @_;
 	print "ERROR: $msg\n";
-	unlink $tmpfile;
 	exit;
 }
 
 # sort version numbers correctly
 sub cmpver {
 	my ($a, $b) = @_;
-	
+
 	# split version parts: 1.2.3rc1 --> 1  2  3rc1
 	my @a_parts = split /\./, $a;
 	my @b_parts = split /\./, $b;
-	
+
 	for (my $i=0; $i<scalar(@a_parts); $i++) {
 		return 1 if $i >= scalar(@b_parts);
 		# split number parts: 3rc1 --> 3  rc  1
@@ -50,9 +52,9 @@ sub cmpver {
 		return -1 if     $a_type and not $b_type;
 		return  1 if not $a_type and     $b_type;
 		# a1 before b1
-		$cmp = $a_type cmp $b_type; return $cmp unless $cmp == 0;
+		$cmp = ($a_type//"") cmp ($b_type//""); return $cmp unless $cmp == 0;
 		# rc1 before rc2
-		$cmp = $a_idx <=> $b_idx;   return $cmp unless $cmp == 0;
+		$cmp = ($a_idx//0) <=> ($b_idx//0);   return $cmp unless $cmp == 0;
 	}
 	# 1.2 before 1.2.1
 	return -1 if scalar(@a_parts) < scalar(@b_parts);
@@ -60,28 +62,43 @@ sub cmpver {
 	return 0;
 }
 
-# Check for subversion client
-my $svn = `which svn`;
-stop("No subversion client found") unless $svn;
+# Check for Git client
+`which git` or stop("No Git client found");
 
-# List tags from subversion repo
-my @tags = `svn ls 'svn://svn.zabbix.com/tags'`;
-chomp @tags; # remove trailing newline
-@tags = (map { $_ =~ s/\/$//; $_ } @tags); # remove trailing slash
-@tags = sort { cmpver($a,$b) } @tags;
+# Get tag list from repo:
+#	7f6b20903537b9bbf72fe2b75ab7fac557856aad	refs/tags/1.0
+#	693709cc4a80777f7759856c853b38cbc920f068	refs/tags/1.1
+print "Querying existing tags from $REPO...\n";
+my @tags_raw = `git ls-remote -t $REPO`;
+# remove trailing newline
+chomp @tags_raw;
+# skip release candidates, betas and tags like "zabicom-xxx"
+@tags_raw = grep { m{ refs/tags/ \d+ \. \d+ ( \. \d+ )? $}x } @tags_raw;
 
-for my $tag (@tags) {
+# Create HashRef:
+#   1.0 => 7f6b20903537b9bbf72fe2b75ab7fac557856aad
+#   1.1 => 693709cc4a80777f7759856c853b38cbc920f068
+my $tags = { map { m{^ (\w+) \s+ refs/tags/ (.*) $}x; $2 => $1 } @tags_raw };
+
+# Loop over tags and read table schema
+print "Reading table schemas...\n";
+for my $tag (sort { cmpver($a,$b) } keys %$tags) {
 	next if cmpver($tag, "1.3.1") < 0; # before Zabbix 1.3.1, schema was stored as pure SQL
 
 	my $schema;
 	my $subdir;
 
-	printf "%-10s %s", $tag, "Searching schema...";
+	printf " - %-8s %s", $tag, "Looking for schema...";
 	# search in subdir /schema (<= 1.9.8) and /src for schema.(sql|tmpl)
 	for my $sub (qw(schema src)) {
-		my @files = `svn ls 'svn://svn.zabbix.com/tags/$tag/create/$sub' 2>/dev/null`;
-		next unless @files; # directory not found?
-		chomp @files;
+		# file list:
+		#	100644 blob 1f0a05eb826dfcdb26f9429ad30c720454374ca1	data.tmpl
+		#	100644 blob b98b5eecc62731508c09d9e76d7aed9d4eb201f2	schema.tmpl
+		my @files_raw = `curl -s $REPO_WEB/create/$sub?at=refs%2Ftags%2F$tag`;
+		next unless @files_raw; # directory not found?
+		chomp @files_raw; # remove trailing newline
+		my @files = map { /^ \d+ \s+ \w+ \s+ \w+ \s+ (.*) $/x; $1 } @files_raw;
+
 		($schema) = grep /^schema\.(sql|tmpl)/, @files;
 		$subdir = $sub;
 		last;
@@ -90,10 +107,9 @@ for my $tag (@tags) {
 		print "\nNo schema found in tag $tag\n";
 		next;
 	}
-	print " ($schema) Download... ";
-	system("svn --force export svn://svn.zabbix.com/tags/$tag/create/$subdir/$schema $tmpfile >/dev/null");
-	open my $fh, '<', $tmpfile or stop("Couldn't open temp file: $!"); 
-	while (<$fh>) {
+	print " Processing ($schema)... ";
+	my @table = `curl -s $REPO_WEB/create/$subdir/$schema?at=refs%2Ftags%2F$tag`;
+	for (@table) {
 		chomp;
 		next unless m/^TABLE/;
 		my (undef, $table) = split /\|/;
@@ -103,14 +119,12 @@ for my $tag (@tags) {
 	print " Done\n";
 }
 
-unlink $tmpfile;
-
 #
 # Print out results
 #
 print "\n\n";
-print "TABLE                     FIRST USE    LAST USE\n";
-print "-------------------------------------------------\n";
+print "TABLE                      FIRST USE  LAST USE\n";
+print "------------------------------------------------\n";
 for my $tab (sort keys %$tables) {
-	printf "%-25s %-8s - %s\n", $tab, $tables->{$tab}->[0], $tables->{$tab}->[-1];
+	printf "%-26s %-8s - %s\n", $tab, $tables->{$tab}->[0], $tables->{$tab}->[-1];
 }
